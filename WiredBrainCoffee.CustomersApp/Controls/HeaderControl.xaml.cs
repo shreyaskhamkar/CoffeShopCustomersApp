38be306b9be292c3191fe8b29620d08e6abd3a4b
shreyaskhamkae <khamk@SHREYAS-PC>
2023-02-04 11:17:51 +0530

    Add project files.

---
+++ WiredBrainCoffee.CustomersApp/Controls/HeaderControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace WiredBrainCoffee.CustomersApp.Controls
+{
+    public partial class HeaderControl : UserControl
+  {
+    public HeaderControl()
+    {
+      InitializeComponent();
+    }
+  }
+}
