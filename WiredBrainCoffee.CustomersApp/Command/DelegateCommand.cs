17c40099233dbdc78cb9a67cb5d89eceb322a743
shreyaskhamkae <khamk@SHREYAS-PC>
2023-02-04 12:31:15 +0530

    feat: commands

---
+++ WiredBrainCoffee.CustomersApp/Command/DelegateCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WiredBrainCoffee.CustomersApp.Command
+{
+    public class DelegateCommand : ICommand
+    {
+        private Action<object?> _execute;
+        private Func<object?, bool>? _canExecute;
+        private DelegateCommand? deleteCommand;
+
+        public DelegateCommand(Action<object?>execute, Func<object?, bool>? canExecute = null)
+  
+        {
+            ArgumentNullException.ThrowIfNull(execute);
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public DelegateCommand(DelegateCommand? deleteCommand)
+        {
+            this.deleteCommand = deleteCommand;
+        }
+
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this,EventArgs.Empty);
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter) => _canExecute is null || _canExecute(parameter);
+     
+        //clean execute method
+        public void Execute(object? parameter) => _execute(parameter);
+    }
+
+
+}
